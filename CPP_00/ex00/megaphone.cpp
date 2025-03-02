b30a8e09ef022ee4d34b26d413c66ffbe22938c0
rcosta-c <ruben.carvalho@gmail.com>
2025-03-02 22:42:53 +0000

    00_ex00

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	int x;
+	int xx;
+
+	xx = 0;
+	x = 1;
+
+	if(ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	while(x < ac)
+	{
+		while(av[x] && av[x][xx] && x < ac)
+		{
+			std::cout << (char) std::toupper(av[x][xx]);
+			xx++;
+		}
+		x++;
+		xx = 0;
+	}
+	std::cout << std::endl;
+	return 0;
+}
