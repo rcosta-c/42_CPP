2f3e7e99d5fd12c8c86870158d3e176532769157
rcosta-c <ruben.carvalho@gmail.com>
2025-04-15 00:34:54 +0100

    update header

--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcosta-c <rcosta-c@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 00:33:13 by rcosta-c          #+#    #+#             */
+/*   Updated: 2025/04/15 00:33:17 by rcosta-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 
 int main(int ac, char **av)
@@ -12,8 +24,3 @@ int main(int ac, char **av)
     return (0);
  
 }
-// This code is a simple command-line program that takes a single argument (a string representing a level) and uses the Harl class to complain about that level.
-// The program first checks if the correct number of arguments is provided (2 arguments: the program name and the level).
-// If not, it prints a usage message and returns an error code (1).
-// If the correct number of arguments is provided, it creates an instance of the Harl class and calls the complain method with the provided level.
-// The Harl class is expected to have a method called complain that takes a string argument and handles the complaint based on the level.   
